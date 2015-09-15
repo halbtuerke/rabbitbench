e0ebffba23fbb982a1c417fe5ebfcb9236bd7304
Patrick Mosby <info@schreiblogade.de>
2015-09-15 12:46:35 +0200

    Add benchmarks with more load.

--- scenario-arm6-simple/spec.js
+++ scenario-arm6-simple/spec.js
@@ -1,22 +1,45 @@
 [{
-	'name': 'consume 1->1 armv61', 
-	'type': 'simple', 
+	'name': 'consume 1->1 armv61',
+	'type': 'simple',
 	'params': [{
-		'time-limit': 30, 
-		'producer-count': 1, 
-		'consumer-count': 1 
+		'time-limit': 30,
+		'producer-count': 1,
+		'consumer-count': 1
 	}],
-	'uri': 'amqp://mq:mq@rpi01'
+	'uri': 'amqp://guest:guest@black-pearl'
 },
 
 {
-	'name': 'consume 1->2 armv61', 
-	'type': 'simple', 
+	'name': 'consume 1->2 armv61',
+	'type': 'simple',
 	'params': [{
-		'time-limit': 30, 
-		'producer-count': 1, 
-		'consumer-count': 2 
+		'time-limit': 30,
+		'producer-count': 1,
+		'consumer-count': 2
 	}],
-	'uri': 'amqp://mq:mq@rpi01'
-}]
+	'uri': 'amqp://guest:guest@black-pearl'
+},
 
+{
+	'name': 'consume 5->5 armv61',
+	'type': 'simple',
+	'params': [{
+		'time-limit': 30,
+		'producer-count': 5,
+		'consumer-count': 5
+	}],
+	'uri': 'amqp://guest:guest@black-pearl'
+},
+
+{
+    'name': 'consume 7->7 armv61',
+	'type': 'simple',
+	'params': [{
+		'time-limit': 30,
+		'producer-count': 7,
+		'consumer-count': 7
+	}],
+	'uri': 'amqp://guest:guest@black-pearl'
+}
+
+]
